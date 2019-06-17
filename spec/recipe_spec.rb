9d7655314c0833b18da416d128faf502e4d35377
Tino Calancha <tino.calancha@gmail.com>
2019-06-18 02:43:24 +0900

    Add tests to define the class expected readers
    The snippet code shows that the class must have readers
    :name, :ingredients

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,2 +1,19 @@
 require 'recipe'
 
+# Recipe class has instance readers :name, :ingredients
+describe Recipe do
+  describe '#name' do
+    context 'the instance method "name"' do
+      it 'returns the recipe name' do
+        expect(Recipe.new('pizza').name).to eql('pizza')
+      end
+    end
+
+    context 'the instance method "ingredients"' do
+      it 'returns the recipe ingredients' do
+        expect(Recipe.new('pizza', %w[bread cheese tomato]).ingredients).to eql(%w[bread cheese tomato])
+      end
+    end
+
+  end
+end
