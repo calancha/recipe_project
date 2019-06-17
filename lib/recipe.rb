ebf2e33fe66af918288871a2f7448127a0963ba7
Tino Calancha <tino.calancha@gmail.com>
2019-06-18 02:33:16 +0900

    Initialize the Git repository

---
+++ lib/recipe.rb
@@ -0,0 +1,34 @@
+################################################################################
+###  Build a Recipe class so that the following code works
+
+# Recipe.describe do
+#   recipe 'Hot Cake' do
+#     ingredient 'Egg'
+#     ingredient 'Flour'
+#     ingredient 'Water'
+#     ingredient 'Sugar'
+#   end
+
+#   recipe 'Miso Soup' do
+#     ingredient 'Tofu'
+#     ingredient 'Green Chard'
+#     ingredient 'Green Onion'
+#     ingredient 'White miso paste'
+#   end
+# end
+
+# hotcake = Recipe.for('Hot Cake')
+# puts hotcake.name
+# # => Hot Cake
+# puts hotcake.ingredients.inspect
+# # => ["Egg", "Flour", "Water", "Sugar"]
+
+# soup = Recipe.for('Miso Soup')
+# puts soup.name
+# # => Miso Soup
+# puts soup.ingredients.inspect
+# # => ["Tofu", "Green Chard", "Green Onion", "White miso paste"]
+
+################################################################################
+class Recipe
+end
