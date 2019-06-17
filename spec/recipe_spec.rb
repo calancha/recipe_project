578324a7c3ffcf4231f7655995a0633525c70adb
Tino Calancha <tino.calancha@gmail.com>
2019-06-18 02:59:21 +0900

    Add test specifying instance 'ingredient' method

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,19 +1,45 @@
 require 'recipe'
 
+# Add instance variables to share in the tests
+RSpec.configure do |config|
+  config.before(:example) do
+    @recipe_name = 'pizza'
+    @pizza_ingredients = %w[bread cheese tomato]
+    @pizza = Recipe.new(@recipe_name, @pizza_ingredients)
+  end
+end
+
 # Recipe class has instance readers :name, :ingredients
 describe Recipe do
   describe '#name' do
     context 'the instance method "name"' do
       it 'returns the recipe name' do
-        expect(Recipe.new('pizza').name).to eql('pizza')
+        expect(@pizza.name).to eql(@recipe_name)
       end
     end
 
     context 'the instance method "ingredients"' do
       it 'returns the recipe ingredients' do
-        expect(Recipe.new('pizza', %w[bread cheese tomato]).ingredients).to eql(%w[bread cheese tomato])
+        expect(@pizza.ingredients).to eql(@pizza_ingredients)
       end
     end
 
+  end
+end
+
+# Method ingredient appends a given ingredient to its recipe
+describe Recipe do
+  describe '#ingredient' do
+    before(:context) do
+      @hawaian_pizza = Recipe.new('hawaian pizza', @pizza_ingredients)
+      extra_ingredient = 'pineapple' # Sorry hawaians!
+      @hawaian_pizza.ingredient(extra_ingredient)
+      @hawaian_pizza_ingredientes = @pizza_ingredients + [extra_ingredient]
+    end
+    context 'the instance method "ingredient"' do
+      it 'adds a new ingredient to its recipe' do
+        expect(@hawaian_pizza.ingredients).to eql(@hawaian_pizza_ingredients)
+      end
+    end
   end
 end
